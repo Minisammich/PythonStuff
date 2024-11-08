2db1d616826351b2118c81229e1b698eca6cff74
Minisammich <91817698+Minisammich@users.noreply.github.com>
2024-11-08 14:59:04 -0600

    Newton's Method

--- CMPT142/test.py
+++ CMPT142/test.py
@@ -1,2 +1,11 @@
-l = [4,6] + [1,2]
-print(l)
+def f(x):
+    return((x**3)-(2*x)-5)
+
+def fp(x):
+    return(3*(x**2)-2)
+
+x1 = 2
+for i in range(5):
+    print(f(x1))
+    x2 = x1-(f(x1)/fp(x1))
+    x1 = x2
